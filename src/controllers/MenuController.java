9d45adbcfa972458a9124f80ba875f00433fafa5
kielypatrick <kiely.patrick@gmail.com>
2017-05-20 16:47:35 +0100

    Add files via upload
    Developing menu options. Adding/Updating members

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 import controllers.GymApi;
 import models.Member;
+import models.Person;
 import models.PremiumMember;
 import models.StudentMember;
+import utils.Analytics;
 import utils.ScannerInput;
 
 import java.util.ArrayList;
@@ -68,7 +70,7 @@ public class MenuController
     public MenuController()
     {
         try {
-            gym.load();
+            gym.loadMember();
         }
         catch (Exception e) {System.err.println("Error reading from file: " + e);
         }
@@ -79,6 +81,7 @@ public class MenuController
         System.out.println("Welcome to Elite Fitness\n");
 
         gym.addMembers();
+        gym.addTrainers();
 
         runWelcome();
 
@@ -142,9 +145,8 @@ public class MenuController
     	System.out.println("Are you a member or trainer?");
 		System.out.println("1: Member");
 		System.out.println("2: Trainer");
-        System.out.println("\n  0: Exit");
-        System.out.print("==>> ");
-        int option = input.nextInt();
+        System.out.println("\n  0: Exit\n");
+        int option = validNextInt("==>> ");
         return option;
     }
     
@@ -172,18 +174,30 @@ public class MenuController
                     } else {
 
                         System.out.println("\nWelcome " + gym.searchMembersByEmail(emailEntered).getMemberName() + "!");
-                      //  runMemberMenu(loggedInMember);
+                        runMemberMenu(loggedInMember);
                     }
 
                     break;
                 case 2:
-                	System.out.println("Please enter email address..");
-                    System.out.print("\tEmail: ");
-                    String email = input.nextLine();
+                    System.out.println("Please enter email address..");
+                    String temailEntered = validNextString("Enter email:");
                     //gym  call the search method in the gymapi
-                    gym.searchTrainersByEmail(email);
-                    
+
+                    Person loggedInTrainer = gym.searchTrainersByEmail(temailEntered);
+
+                    if (gym.searchTrainersByEmail(temailEntered) == null)
+
+                    {
+                        System.out.println("Access Denied");
+                        System.exit(0);
+                    } else {
+
+                        System.out.println("\nWelcome " + gym.searchTrainersByEmail(temailEntered).getMemberName() + "!");
+                        runTrainerMenu(loggedInTrainer);
+                    }
+
                     break;
+                    
                 default:
 					System.out.println("Invalid option entered: " + loginOption);
 					break;
@@ -206,8 +220,8 @@ public class MenuController
         System.out.println("1: New Member");
         System.out.println("2: New Trainer");
         System.out.println("\n  0: Exit");
-        System.out.print("==>> ");
-        int option = input.nextInt();
+
+        int option = validNextInt("==>> ");
         return option;
     }
     
@@ -238,6 +252,11 @@ public class MenuController
                         else {
                             gym.addMember(new PremiumMember(email, name, address, gender, height, weight, packag));
                         }
+                        try {
+                            gym.saveMember();
+                        } catch (Exception e) {
+                            System.err.println("Error writing to file: " + e);
+                        }
 
                             break;
                     case 2:
@@ -253,8 +272,239 @@ public class MenuController
                                 break;
 
                 }
+
+                //pause the program so that the user can read what we just printed to the terminal window
+                System.out.println("\nPress any key to continue...");
+                input.nextLine();
+                input.nextLine();  //this second read is required - bug in Scanner class; a String read is ignored straight after reading an int.
+
+                //display the main menu again
+                runWelcome();
+
             }
+
+        //the user chose option 0, so exit the program
+        System.out.println("Exiting... bye");
+        System.exit(0);
     }
+
+    private int memberMenu() {
+        System.out.println("\t1.View profile\n" +
+                "\t2. Update profile\n" +
+                "\t3. View Progress:" +
+                "\n\t0. Logout");
+        int memberMenuOption = validNextInt("==>> ");
+        return memberMenuOption;
+    }
+
+    private void runMemberMenu(Member member) {
+        int option = memberMenu();
+        while (option != 0) {
+
+
+            switch (option) {
+
+                case 1:
+                    double bmi = Analytics.calculateBMI(member);
+
+                    System.out.print("Name:\t" + member.getMemberName()
+                            + "\nHeight:\t" + member.getHeight()
+                            + "\nBMI:\t" + Analytics.calculateBMI(member)
+                            + "\nBMI Category:\t" + Analytics.determineBMICategory(bmi)
+                            + "\nIdeal Weight:\t" + Analytics.isIdealBodyWeight(member)
+                    );
+
+                    break;
+
+                case 2:
+                    runMemberUpdate(member);
+
+                    break;
+
+                case 3:
+                    //runMemberProgress(member);
+                 //   System.out.println(member.assessments.size());
+
+
+                    break;
+                default:
+                    System.out.println("Invalid option entered: " + option);
+                    break;
+            }
+            //pause the program so that the user can read what we just printed to the terminal window
+            System.out.println("\nPress any key to continue...");
+            input.nextLine();
+            input.nextLine();  //this second read is required - bug in Scanner class; a String read is ignored straight after reading an int.
+
+            runMemberMenu(member);
+        }
+        //the user chose option 0, so exit the program
+        System.out.println("Exiting... bye");
+        System.exit(0);
+    }
+
+    private int memberUpdate() {
+        System.out.println("What do you wish to update?");
+        System.out.println("\t1. Name\n" +
+                "\t2. Address\n" +
+                "\t3. Gender\n" +
+                "\t4. Email\n" +
+                "\n\t 0. Return the Member Menu");
+        int memberUpdateOption = validNextInt("==>> ");
+        return memberUpdateOption;
+    }
+
+    private void runMemberUpdate(Member member)
+    {
+        int option = memberUpdate();
+        while (option != 0) {
+
+
+            switch (option) {
+
+                case 1:
+                    String newName = validNextString(" New Name?");
+                    member.setMemberName(newName);
+
+                    break;
+
+                case 2:
+                    String newAddress = validNextString(" New Address:");
+                    member.setMemberAddress(newAddress);
+                    break;
+
+                case 3:
+
+                    String newGender = validNextString(" New Gender:");
+                    member.setGender(newGender);
+                    break;
+
+                case 4:
+                    String newEmail = validNextString(" New email:");
+                    member.setMemberEmail(newEmail);
+                    break;
+
+                default:
+                    System.out.println("Invalid option entered: " + option);
+                    break;
+            }
+            try {
+                gym.saveMember();
+            }
+            catch (Exception e) {
+                System.err.println("Error writing to file: " + e);
+            }
+            //pause the program so that the user can read what we just printed to the terminal window
+            System.out.println("\nPress any key to continue...");
+            input.nextLine();
+            input.nextLine();  //this second read is required - bug in Scanner class; a String read is ignored straight after reading an int.
+
+            runMemberMenu(member);
+        }
+    }
+
+
+    private int trainerMenu() {
+        System.out.println("\t1. Add Member\n" +
+                "\t2. List ALL Members\n" +
+                "\t3. Search Members by Email\n" +
+                "\t4. Search Members by Name\n" +
+                "\t5. List Members with Ideal Body Weight\n" +
+                "\t6. List Members by BMI Category\n" +
+                "\t7. Assessments\n" +
+                "\t8. Reports\n" +
+
+                "\n\t0. Logout");
+
+        int trainerMenuOption =  validNextInt("==>> ");
+        return trainerMenuOption;
+    }
+
+    private void runTrainerMenu(Person trainer) {
+        int trainerOption = trainerMenu();
+        while (trainerOption != 0) {
+
+
+            switch (trainerOption) {
+
+                case 1:
+
+                    String email = validNextString("Enter email:\t");
+                    String name = validNextString("Name:\t");
+                    String address = validNextString("Address:\t");
+                    String gender = validNextString("Gender: (m/f)\t"	);
+                    Double height = validNextDouble("Height:\t");
+                    Double weight = validNextDouble("Weight:\t");
+                    String packag = validNextString("Package:\t");
+                    String memberType = validNextString("Is the member a Student? (Y/N)");
+                    if (memberType.equals("Y") || memberType.equals("y")) {
+                        String college = validNextString("College:\t");
+                        Integer studentId = validNextInt("Student ID Number:\t");
+                        gym.addMember(new StudentMember(email, name, address, gender, height, weight, packag, studentId, college));
+                    } else {
+                        gym.addMember(new PremiumMember(email, name, address, gender, height, weight, packag));
+                    }
+                    try {
+                        gym.saveMember();
+                    } catch (Exception e) {
+                        System.err.println("Error writing to file: " + e);
+                    }
+
+                    break;
+
+                case 2:
+                    System.out.println(gym.getMembers());
+
+
+                    break;
+
+                case 3:
+                    String searchEmail = validNextString("Enter email:\t");
+                    System.out.println(gym.searchMembersByEmail(searchEmail));
+
+                    break;
+
+                case 4:
+                    String searchName = validNextString("Enter Name:\t");
+                    System.out.println(gym.searchMembersByName(searchName));
+
+                    break;
+
+                case 5:
+
+                    System.out.println(gym.listMembersWithIdealWeight());
+
+                    break;
+
+                case 6:
+                    //runBMICatSearch(trainer);
+
+                    break;
+
+                case 7:
+                    //runAssessments(trainer);
+                    break;
+
+
+                default:
+                    System.out.println("Invalid option entered: " + trainerOption);
+                    break;
+            }
+            //pause the program so that the user can read what we just printed to the terminal window
+            System.out.println("\nPress any key to continue...");
+            input.nextLine();
+            input.nextLine();  //this second read is required - bug in Scanner class; a String read is ignored straight after reading an int.
+
+            runTrainerMenu(trainer);
+        }
+        //the user chose option 0, so exit the program
+        System.out.println("Exiting... bye");
+        System.exit(0);
+    }
+
+
+
+
 }
 
     
